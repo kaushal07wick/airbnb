132e6bee0f6c710b3a6fe3276164d2d78f43355f
kaushal Choudhary <kaushalc64@gmail.com>
2024-09-21 15:44:05 +0000

    fixes: added_login_class

--- app/javascript/controllers/wishlist_controller.js
+++ app/javascript/controllers/wishlist_controller.js
@@ -1,15 +1,27 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  updateWishlistStatus(){
-    if(this.element.dataset.status === "false"){
-        this.element.classList.remove("fill-none")
-        this.element.classList.add("fill-primary")
-        this.element.dataset.status = "true";
-    }else{
-        this.element.classList.remove("fill-primary")
-        this.element.classList.add("fill-none")
-        this.element.dataset.status = "false";
+
+  updateWishlistStatus() {
+    // Get status if user is logged-in
+    // If logged-out, redirect to login page, return
+    // else continue
+
+    const isUserLoggedIn = this.element.dataset.userLoggedIn;
+    if(isUserLoggedIn === "false"){
+      document.querySelector(".js-login").click();
+      return
+    }
+
+    if (this.element.dataset.status === "false"){
+      this.element.classList.remove("fill-none");
+      this.element.classList.add("fill-primary");
+      this.element.dataset.status = "true";
+    }
+    else {
+      this.element.classList.remove("fill-primary");
+      this.element.classList.add("fill-none");
+      this.element.dataset.status = "false";
     }
   }
 }
