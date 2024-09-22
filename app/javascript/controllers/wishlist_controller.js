f2792f8df123c269c58ce24bfafb7862434cc282
kaushal Choudhary <kaushalc64@gmail.com>
2024-09-22 21:17:02 +0530

    fixed_wishlists

--- app/javascript/controllers/wishlist_controller.js
+++ app/javascript/controllers/wishlist_controller.js
@@ -14,14 +14,66 @@ export default class extends Controller {
     }
 
     if (this.element.dataset.status === "false"){
+      const userId = this.element.dataset.userId;
+      const propertyId = this.element.dataset.propertyId;
+      this.addPropertyToWishlist(propertyId, userId)
+    }
+    else {
+      const wishlistId = this.element.dataset.wishlistId;
+      this.removePropertyFromWishlist(wishlistId)
+    }
+  }
+
+  
+  addPropertyToWishlist(property_id, user_id){
+    const params = {
+      property_id: property_id,
+      user_id: user_id,
+    };
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body:  JSON.stringify(params)
+    };
+
+    fetch('/api/wishlists', options)
+    .then(response => {
+      if(!response.ok){
+        
+        console.log(response.status)
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      console.log(data.id)
+      this.element.dataset.wishlistId = data.id;
       this.element.classList.remove("fill-none");
       this.element.classList.add("fill-primary");
       this.element.dataset.status = "true";
-    }
-    else {
+
+    })
+    .catch(e => {
+      console.log(e);
+    });
+  }
+
+
+  removePropertyFromWishlist(wishlistId) {
+    fetch('/api/wishlists/' + wishlistId, {
+      method: 'DELETE',
+    })
+    .then(response => {
+      this.element.dataset.wishlistId = '';
       this.element.classList.remove("fill-primary");
       this.element.classList.add("fill-none");
       this.element.dataset.status = "false";
-    }
+    })
+    .catch(e => {
+      console.log(e);
+    });
   }
 }
