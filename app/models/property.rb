d97f8ffbe7804267ba1ddcf0cac41702800deb1d
kaushal Choudhary <kaushalc64@gmail.com>
2024-09-22 20:19:59 +0530

    minor_changes_to_wishlist_api

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,25 +1,29 @@
 class Property < ApplicationRecord
-  # name, headline, description, address_1, city, state, country
   validates :name, presence: :true
-  validates :description, presence: :true
   validates :headline, presence: :true
+  validates :description, presence: :true
   validates :address_1, presence: :true
   validates :city, presence: :true
   validates :state, presence: :true
   validates :country, presence: :true
+
   monetize :price_cents, allow_nil: true
 
   has_many_attached :images
-  
+
   has_many :reviews, dependent: :destroy
 
   has_many :wishlists, dependent: :destroy
+  has_many :wishlisted_users, through: :wishlists, source: :user, dependent: :destroy
 
-  has_many :wishlisted_users, through: :wishlist, source: :user,  dependent: :destroy
-
-  def update_average_final_rating
+  def update_average_rating
     average_rating = reviews.average(:final_rating);
-      update_column(:average_final_rating, average_rating)
+    update_column(:average_final_rating, average_rating)
+  end
 
+  def wishlisted_by?(user = nil)
+    return if user.nil?
+
+    wishlisted_users.include?(user)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
 
   has_many :wishlists, dependent: :destroy
 
-  has_many :wishlisted_properties, through: :wishlist, source: :property,  dependent: :destroy
+  has_many :wishlisted_properties, through: :wishlists, source: :property,  dependent: :destroy
 end
