b18f3ad99d2334b08d7e00a395fc4f08703d9590
kaushal Choudhary <kaushalc64@gmail.com>
2024-10-03 18:40:01 +0530

    actiontext

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+description = <<-DESCRIPTION
+<h1 class="font-medium">Hi. It&rsquo;s me, Seth!</h1>
+<div>If you&rsquo;re reading this, you&rsquo;re considering booking a night at the Houseplant-inspired retreat. What makes it Houseplant-y, you may be asking? Well, it&rsquo;s stocked with some of our favourite Housegoods (even a pre-release copy of our NEW Vinyl Box Set Vol. 2!). The mid-century styling and LA views also have a distinctly Houseplant feel. And there&rsquo;s the fact that I&rsquo;ll be there to welcome you which, given that I&rsquo;m the founder, adds to the Houseplant-ness. I AM HOUSEPLANT!!!</div>
+<h2 class="font-medium">What you&rsquo;ll do</h2>
+<p>This mid-century modern space is an ideal getaway if you&rsquo;re looking for inspiration or just for a good hangout. There&rsquo;s also a pottery wheel there.<br />During your stay, you will get a glimpse into my creative process with opportunities to:<br />&ndash; Throw some pottery with ME in the in-house studio. I&rsquo;m a pretty good teacher!<br />&ndash; Take in the sun, the view and the setting, all equally good whether you&rsquo;re looking to be relaxed or inspired.<br />&ndash; Listen to Vol. 2 of our Vinyl Box Set before it comes out. Three vinyl records with tracklists curated by me and Evan Goldberg to match whatever mood you&rsquo;re in.<br />&ndash; Enjoy a fully stocked fridge, by which I mean go ahead and destock it/eat it all.<br />Booking opens for US residents at 10.00am PT on 7 Feb for three one-night stays on 15, 16 and 17 Feb.<br />Also, as part of our collaboration with Airbnb, they&rsquo;ll be making a one-time donation to Hilarity for Charity, a national non-profit on a mission to care for families affected by Alzheimer&rsquo;s disease, to activate the next generation of Alzheimer&rsquo;s advocates and to be a leader in brain health research and education.</p>
+<h4>Other things to note</h4>
+<div>Following arrival and check-in, our on-site concierge will help ensure a comfortable stay.<br />Following public health guidance, guests and on-site staff are required to comply with local rules regarding COVID-19 as well as Airbnb&rsquo;s COVID-19 Safety Practices.<br />These one-night stays are not a contest. Guests are responsible for their own travel to and from Los Angeles. This home is privately owned and operated. No cannabis-based products will be made available or provided to guests during the stays.
+Listing is not available for booking by nationalities prohibited by local law.</div>
+<h4 class="font-medium">Registration number</h4>
+<p>HSR22-004196</p>
+DESCRIPTION
 
 user = User.create!({
     email: 'test1@gmail.com',
@@ -16,7 +27,7 @@ user = User.create!({
 5.times do |i|
     property = Property.create!({
         name: Faker::Lorem.unique.sentence(word_count: 3),
-        description: Faker::Lorem.paragraph(sentence_count: 10),
+        description: description,
         headline: Faker::Lorem.unique.sentence(word_count: 3),
         address_1: Faker::Address.street_address,
         address_2: Faker::Address.street_name,
