c1ebf64ccf4ed6101c51f1c471bf6bd2cc386988
kaushal Choudhary <kaushalc64@gmail.com>
2024-11-28 06:38:24 +0530

    booking-ui

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,9 @@
 class BookingsController < ApplicationController
     def new
         @property = Property.find(params[:property_id])
-        @checkin_date = params[:checkin_date]
-        @checkout_date = params[:checkout_date]
-        @total_nights = numberOfNights()
+        @checkin_date = Date.parse(params[:checkin_date])
+        @checkout_date = Date.parse(params[:checkout_date])
+        @total_nights = numberOfNights
         @base_fare = @property.price * @total_nights
         @service_fee = @base_fare * 0.18
         @total_amount = @base_fare + @service_fee
@@ -14,7 +14,7 @@ class BookingsController < ApplicationController
     def numberOfNights
         checkin_date = Date.parse(params[:checkin_date])
         checkout_date = Date.parse(params[:checkout_date])
-        return (checkout_date - checkin_date).to_i
+        (checkout_date - checkin_date).to_i
     end
 
     def booking_params
