ca3f29e60ac21cb8cf346a5de5948cfa71a497ae
kaushal Choudhary <kaushalc64@gmail.com>
2024-11-28 12:34:23 +0530

    stripe-backend-part55

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,23 +1,27 @@
 class BookingsController < ApplicationController
+    before_action :authenticate_user!
+    
     def new
-        @property = Property.find(params[:property_id])
-        @checkin_date = Date.parse(params[:checkin_date])
-        @checkout_date = Date.parse(params[:checkout_date])
-        @total_nights = numberOfNights
-        @base_fare = @property.price * @total_nights
-        @service_fee = @base_fare * 0.18
-        @total_amount = @base_fare + @service_fee
+      @property = Property.find(params[:property_id])
+      @checkin_date = Date.parse(params[:checkin_date])
+      @checkout_date = Date.parse(params[:checkout_date])
+  
+      @total_nights = numberOfNights
+  
+      @base_fare = @property.price * @total_nights
+      @service_fee = @base_fare * 0.18
+      @total_amount = @base_fare + @service_fee
     end
-
+  
     private
-
+  
     def numberOfNights
-        checkin_date = Date.parse(params[:checkin_date])
-        checkout_date = Date.parse(params[:checkout_date])
-        (checkout_date - checkin_date).to_i
+      checkin_date = Date.parse(params[:checkin_date])
+      checkout_date = Date.parse(params[:checkout_date])
+      (checkout_date - checkin_date).to_i
     end
-
+  
     def booking_params
-        params.permit(:property_id, :checkin_date, :checkout_date)
+      params.permit(:property_id, :checkin_date, :checkout_date)
     end
-end
+  end
