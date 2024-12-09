fcba8f53a851f2577435107db8f3043806bbdd6d
kaushal Choudhary <kaushalc64@gmail.com>
2024-12-09 15:31:58 +0530

    part82-reservations_backend

--- app/controllers/owner/reservations_controller.rb
+++ app/controllers/owner/reservations_controller.rb
@@ -1,8 +1,8 @@
 module Owner
   class ReservationsController < ApplicationController
     before_action :authenticate_user!
-    def index 
-        @reservations = current_user.reservations
+    def index
+        @reservations = current_user.properties.map(&:reservations).flatten
     end
   end
 end
