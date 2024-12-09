da53772f2a88b9f5ac442bed8bf4357fb7c9d75a
kaushal Choudhary <kaushalc64@gmail.com>
2024-12-09 15:08:37 +0530

    ui_fix_for_mobile_menu

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -3,17 +3,26 @@ import {enter, leave, toggle} from 'el-transition'
 
 export default class extends Controller {
 
-  static targets =['dropdown', 'openUserMenu']
+  static targets =['dropdown', 'openUserMenu', 'dropdownMob', 'openUserMenuMob']
 
   connect () {
     this.openUserMenuTarget.addEventListener("click", (e)=> {
       openDropdown(this.dropdownTarget)
+    })
+    this.openUserMenuMobTarget.addEventListener("click",(e) =>{
+      openDropdownMob(this.dropdownMobTarget)
     })
   }
 }
 
 function openDropdown(element) {
   toggle(element).then(() => {
+  })
+}
+
+function openDropdownMob(element){
+  toggle(element).then(() =>{
+
   })
 }
 
